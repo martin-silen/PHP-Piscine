40be03e0cc2e30f9b215f0e358b9d72331548508
Martin Silen <msilen@c1r6p8.hive.fi>
2022-04-18 11:56:00 +0300

    updates

--- day05/ex03/ex03.sql
+++ day05/ex03/ex03.sql
@@ -1,8 +1,10 @@
 INSERT INTO ft_table (`login`, `group`, `creation_date`)
+ALTER TABLE ft_table
+ADD `other`()
 SELECT `last_name`, `other` AS `group`, `birthdate`
-FROM `user_card`
+FROM user_card
 WHERE
 		(length(`last_name`) < 9) AND
-		(`last_name` LIKE '%a')
+		(`last_name` LIKE '%a%')
 ORDER BY `last_name` ASC
 LIMIT 10;
