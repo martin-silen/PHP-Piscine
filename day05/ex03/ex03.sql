323f96fae697421b4da046027a209980f55f30a2
Martin Silen <msilen@c1r6p8.hive.fi>
2022-04-18 13:12:13 +0300

    SQL is fun

--- day05/ex03/ex03.sql
+++ day05/ex03/ex03.sql
@@ -1,10 +1,8 @@
 INSERT INTO ft_table (`login`, `group`, `creation_date`)
-ALTER TABLE ft_table
-ADD `other`()
-SELECT `last_name`, `other` AS `group`, `birthdate`
+SELECT `last_name`, 'other' AS `group`, `birthdate`
 FROM user_card
 WHERE
-		(length(`last_name`) < 9) AND
-		(`last_name` LIKE '%a%')
+	(length(`last_name`) < 9) AND
+	(`last_name` LIKE '%a%')
 ORDER BY `last_name` ASC
 LIMIT 10;
