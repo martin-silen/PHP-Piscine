98b461e21fa717f50ece27640f560dd19ce9942d
Martin Silen <martin@silen.io>
2022-04-23 08:34:20 +0300

    Updates

--- day09/ex02/todo.js
+++ day09/ex02/todo.js
@@ -1,97 +1,44 @@
-let cookie;
-let i = 0;
-let new_id = document.cookie.split(";").length;
-if (new_id) {
-for (j = 0; j < 100; j++) {
-	if (cookie = getCookie(j)) {
-		id = cookie.split("=");
-		recreate_to_do(j, id[1]);
-		if (i < id[0])
-		i = id[0];
+let cookies = document.cookie.split(';');
+let cookie_id = cookies.length + 1;
+let parent = document.getElementById('ft_list');
+
+if (cookies)
+{
+	cookies.forEach(function(item){
+		let div = document.createElement('div');
+		let todo = item.split('=');
+		if (!todo[1])
+			return;
+		div.innerHTML = todo[1];
+		div.setAttribute('id', todo[0]);
+		div.setAttribute('onclick', 'delElem(id)');
+		if (todo[0] > cookie_id)
+			cookie_id = Number(todo[0]);
+		parent.insertBefore(div, parent.firstChild);
+	});
+}
+
+function newTask()
+{
+	let task = prompt("Write your new task here:");
+	if (task.trim())
+	{
+		cookie_id++;
+		let div = document.createElement('div');
+		div.innerHTML = task;
+		div.setAttribute('id', cookie_id);
+		div.setAttribute('onclick', 'delElem(id)');
+		parent.insertBefore(div, parent.firstChild);
+		document.cookie = cookie_id + "=" + task + "; expires=Thu, 01 Jan 2021 00:00:00 UTC";
 	}
 }
-i++;
-}
 
-function start() {
-
-	let txt;
-	let thing = prompt("Please enter to do thing:", "Wash dishes");
-	if (thing == null || thing == "") {
-		txt = "User cancelled the prompt.";
-	} else {
-		txt = thing;
-		recreate_to_do(i, txt);
-		i++;
+function delElem(id)
+{
+	if (confirm('Are you sure you want to delete task ' + id))
+	{
+		let elem = document.getElementById(id);
+		elem.parentNode.removeChild(elem);
+		document.cookie = id + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 	}
-}
-
-function del_to_do(id) {
-	if (confirm("Are you sure you want to delete this task?")) {
-		let element = document.getElementById(id);
-		element.parentNode.removeChild(element);
-		eraseCookie(id);
-	}
-}
-
-	function recreate_to_do(id, txt) {
-		// create new elments
-
-		let newParagraph = document.createElement('p');
-		newParagraph.setAttribute("id", id);
-		newParagraph.setAttribute("onclick","del_to_do(id);");
-		setCookie(id, txt, 1);
-		let pText= document.createTextNode(txt);
-
-		newParagraph.appendChild(pText);
-
-		let task = document.getElementById('bla');
-		let parent = task.parentNode;
-
-		parent.insertBefore(newParagraph, parent.firstChild);
-	}
-
-function create_to_do(txt) {
-    // create new elments
-	let newParagraph = document.createElement('p');
-	newParagraph.setAttribute("id", i);
-
-	newParagraph.setAttribute("onclick","del_to_do(id);");
-	setCookie(i, txt, 1);
-	i++;
-
-    let pText= document.createTextNode(txt);
-    newParagraph.appendChild(pText);
-
-    // elements are now created and ready to be added to the DOM.
-
-    // grab element on page you want to add stuff to
-    let task = document.getElementById('ft_list');
-
-	// add both new elements to the page as children to the element we stored in firstHeading.
-
-    // get parent node of firstHeading
-    let parent = task.parentNode;
-
-    // insert newHeading before FirstHeading
-    parent.insertBefore(newParagraph, task);
-}
-
-function setCookie(name,value) {
-	document.cookie = name + "=" + value;
-}
-
-function getCookie(name) {
-    let nameEQ = name + "=";
-    let ca = document.cookie.split(';');
-    for(let i=0;i < ca.length;i++) {
-        let c = ca[i];
-		while (c.charAt(0)==' ') c = c.substring(1,c.length);
-        if (c.indexOf(nameEQ) == 0) return c;
-    }
-    return null;
-}
-
-function eraseCookie(id) {
-    document.cookie = id + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC";
 }
